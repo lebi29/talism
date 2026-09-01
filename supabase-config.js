6c099a6d3905c136fe0cdf53ed351aef6ecdf1a7
lebi29 <ismaillebi29@gmail.com>
2026-09-01 07:19:09 +0300

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,3 @@
 window.TALISM_SUPABASE_URL = 'https://tuahvjarjrpwpyxmqirb.supabase.co';
 window.TALISM_SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_QX_WqNCxnvJVQi39HNGU1Q_YaIQXGC_';
+window.TALISM_AI_FUNCTION_URL = 'https://tuahvjarjrpwpyxmqirb.supabase.co/functions/v1/Ask-TALISM';
